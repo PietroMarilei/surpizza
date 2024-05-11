94988547d47179aae47561a43a820af9ee0b081e
pietro.marilei <pietromrl0@gmail.com>
2024-05-11 15:32:57 +0200

    start svelte ns

---
+++ app-fe/app/components/Home.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let message: string = "Ciao Sono la app della pizza ";
+</script>
+
+<page>
+  <actionBar title="Home" />
+  <gridLayout>
+    <label class="info">
+      <formattedString>
+        <span class="fas" text="&#xf135;" />
+        <span text=" {message}" />
+      </formattedString>
+    </label>
+  </gridLayout>
+</page>
+
+<style>
+  .info .fas {
+    color: #3a53ff;
+  }
+
+  .info {
+    font-size: 20;
+    horizontal-align: center;
+    vertical-align: center;
+  }
+</style>
---
+++ app-fe/webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require("@nativescript/webpack");
+
+module.exports = (env) => {
+	webpack.init(env);
+
+	// Personalizza la configurazione di Webpack
+	webpack.chainWebpack((config) => {
+		// Aggiungi 'svelte' a resolve.conditionNames
+		config.resolve.conditions.add('svelte');
+	});
+
+	return webpack.resolveConfig();
+};
